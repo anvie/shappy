36e84f67ffd69c09d766a27470866149e8912725
robin <robin@ansvia.com>
2011-11-16 03:09:55 +0700

    some fix

--- gitize.sh
+++ gitize.sh
@@ -4,9 +4,9 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.lg=log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
-git config --global alias alias.bu=commit --allow-empty -am "bump update"
-git config --global alias.fixup=commit -am "fixup"
-git config --global alias.crp=cherry-pick
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.bu "commit --allow-empty -am \"bump update\""
+git config --global alias.fixup "commit -am fixup"
+git config --global alias.crp cherry-pick
 
 
