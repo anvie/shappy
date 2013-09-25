de7c7edebd4446f9aebe459dd60f5d6b368d20f0
Robin Syihab <obin.mf@gmail.com>
2013-09-25 17:25:02 +0700

    Update gitize.sh, make git lg show colored graph.

--- gitize.sh
+++ gitize.sh
@@ -4,7 +4,7 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --decorate --color"
 git config --global alias.bu "commit --allow-empty -am \"bump update\""
 git config --global alias.fixup "commit -am fixup"
 git config --global alias.crp cherry-pick
